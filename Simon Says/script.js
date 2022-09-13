eacc4031bfd76f34d908ba535815410347bacc71
Channharith Sakun <111589242+channharith@users.noreply.github.com>
2022-09-13 13:44:05 -0400

    Add files via upload

---
+++ Simon Says/script.js
@@ -0,0 +1,71 @@
+
+// Add in buttons to make sure the game recognizes the buttons
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+
+// Add a way to start the level
+// Increase levels as we continue to press buttons
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+
+
+//Once you click a button there should be a function
+//As the game is tracking the userChosenColor there should be a function to check the Answer
+$(".btn").click(function() {
+
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+//When the game checks the function it should there should be a delay before clicking the next color
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 100);
+      }
+    } else {
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 100);
+
+      startOver();
+    }
+}
+
+//The game should have a RNG to select colors and then it should flash the button 
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  
+}
+
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
